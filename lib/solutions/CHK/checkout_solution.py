8c8d129fb9efb6504d88e23d0e3470a429deb755
sbx_user1051 <sbx_user1051@169.254.87.39>
2025-09-07 00:26:40 +0000

    Sun Sep 07 00:26:40 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,20 @@
 class CheckoutSolution:
 
+    def priceA(self, counter):
+        num5A = counter["A"] // 5
+        num3A = (counter["A"] % 5) // 3
+        num1A = (counter["A"] % 5) % 3
+        return num5A * 200 + num3A * 130 + num1A * 50
+
+    def priceB(self, counter):
+        num2E = counter["E"] // 2
+        num1B = max(counter["B"] - num2E, 0)
+        return (num1B // 2) * 45 + (num1B % 2) * 30
+
+    def priceF(self, counter):
+        num3F = counter["F"] // 3
+        return (counter["F"] - num3F) * 10
+
     # skus = unicode string
     def checkout(self, skus):
         if skus == "": return 0
@@ -11,21 +26,12 @@ class CheckoutSolution:
 
         if len(skus) != sum(counter.values()): return -1
 
-        num5A = counter["A"] // 5
-        num3A = (counter["A"] % 5) // 3
-        num1A = (counter["A"] % 5) % 3
-
-        num2E = counter["E"] // 2
-        num1B = max(counter["B"] - num2E, 0)
-
-        num3F = counter["F"] // 3
-
-        price = num5A * 200 + num3A * 130 + num1A * 50
-        price += (num1B // 2) * 45 + (num1B % 2) * 30
+        price = self.priceA(counter)
+        price += self.priceB(counter)
         price += counter["C"] * 20
         price += counter["D"] * 15
         price += counter["E"] * 40
-        price += (counter["F"] - num3F) * 20
+        price += self.priceF(counter)
 
         return price
 

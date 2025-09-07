f5c037923b2081f40b7f0895b8f5f8cc2dc22b0a
sbx_user1051 <sbx_user1051@169.254.87.39>
2025-09-07 00:14:12 +0000

    Sun Sep 07 00:14:12 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,9 +14,17 @@ class CheckoutSolution:
 
         if len(skus) != sum(counter.values()): return -1
 
-        price = counter["A"] // 3 * 130 + counter["A"] % 3 * 50
-        price += counter["B"] // 2 * 45 + counter["B"] % 2 * 30
+        num5A = counter["A"] // 5
+        num3A = (counter["A"] % 5) // 3
+        num1A = (counter["A"] % 5) % 3
+
+        num2E = counter["E"] // 2
+        num1B = counter["B"] - num2E
+
+        price = num5A * 200 + num3A * 130 + num1A * 50
+        price += (num1B // 2) * 45 + (num1B % 2) * 30
         price += counter["C"] * 20
         price += counter["D"] * 15
 
         return price
+

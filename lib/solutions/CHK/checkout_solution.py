a7ba3ca6b291a1c5f9a4fa422aaf350e17082ff3
sbx_user1051 <sbx_user1051@169.254.87.39>
2025-09-07 00:17:39 +0000

    Sun Sep 07 00:17:39 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,12 +19,13 @@ class CheckoutSolution:
         num1A = (counter["A"] % 5) % 3
 
         num2E = counter["E"] // 2
-        num1B = counter["B"] - num2E
+        num1B = max(counter["B"] - num2E, 0)
 
         price = num5A * 200 + num3A * 130 + num1A * 50
         price += (num1B // 2) * 45 + (num1B % 2) * 30
         price += counter["C"] * 20
         price += counter["D"] * 15
+        price += counter["E"] * 40
 
         return price
 

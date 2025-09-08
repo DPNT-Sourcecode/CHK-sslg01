7d055cfee9f966c7a4eeaac36a03437d34db9945
sbx_user1051 <sbx_user1051@169.254.12.21>
2025-09-08 01:55:14 +0000

    Mon Sep 08 01:55:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -97,6 +97,7 @@ class CheckoutSolution:
             groupDiscountedCounter[group] = sum(skusCounter[sku] for sku in group) // discount[0]
             remaining = groupDiscountedCounter[group] * discount[0]
             for sku in sorted([sku for sku in group], key=lambda x: self.PRICE[x], reverse=True):
+                print(sku, remaining)
                 if remaining == 0:
                     groupDiscountedCounter[sku] = skusCounter[sku]
                     continue
@@ -136,7 +137,8 @@ class CheckoutSolution:
             if count > 0 
         }
         totalPrice = { sku: self.totalSkuPrice(sku) for sku in self.counter.keys() if self.counter[sku] > 0 }
-        
+        print(totalPrice)
+        print(groupDiscountedCounter)
         for group, count in groupDiscountedCounter.items():
             if group in self.GROUP_DISCOUNT:
                 totalPrice[group] = count * self.GROUP_DISCOUNT[group][1]
@@ -147,6 +149,11 @@ class CheckoutSolution:
             if group in self.PRICE:
                 totalPrice[group] = count * self.PRICE[group]
                 
+        print(totalPrice)
         return sum(totalPrice.values())
+
+if __name__ == "__main__":
+    checkout = CheckoutSolution()
+    print(checkout.checkout("SSSZ"))
 
 
